b46d9e1cd8c81cde608fd03694179ab387449412
JimHalpert26 <rahulpadhy.15.09.1996@gmail.com>
2022-12-21 21:44:26 +0530

    Easy, follow up being tricky

---
+++ leetcode/1757.sql
@@ -0,0 +1,8 @@
+-- Approach 1
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
+
+
+
+-- Write a query to get the % of certain products
+select round(avg(case when low_fats = 'Y' or recyclable = 'Y' then 1 else 0 end), 2) 
+as percentage;
