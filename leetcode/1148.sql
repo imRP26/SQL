530bd048921165a1f8f3a15a10eb313e22872d5c
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-01-28 13:42:02 +0530

    Distinct, Having-Case

--- leetcode/1148.sql
+++ leetcode/1148.sql
@@ -5,6 +5,15 @@
 
 
 /*
- * 
+ * My simple, naive solution! 
  */
+select distinct author_id as id from Views where author_id = viewer_id order by id;
 
+
+
+/*
+ * Approach 1 from 
+ * https://leetcode.com/problems/article-views-i/solutions/688022/two-mysql-solutions-with-and-without-distinct/?orderBy=most_votes
+ */
+select author_id as id from Views group by author_id 
+having sum(case when author_id = viewer_id then 1 else 0 end) > 0 order by id;
