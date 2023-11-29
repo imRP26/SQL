ec6503c11bea5228057fc8c94689f33b9040aedf
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-11-29 21:42:07 +0530

    DATE_SUB

--- leetcode/1141.sql
+++ leetcode/1141.sql
@@ -5,16 +5,16 @@
 
 
 /*
- * Simple solution!
+ * Simple Approach!
  */
 select activity_date as day, count(distinct user_id) as active_users from Activity where 
-activity_date > '2019-06-27' and activity_date <= '2019-07-27' group by activity_date;
+activity_date > '2019-06-27' and activity_date <= '2019-07-27' group by activity_date; 
 
 
 
 /*
- * Approach from LC Official Editorial!
+ * Approach from -> 
+ * https://leetcode.com/problems/user-activity-for-the-past-30-days-i/solutions/1961049/just-do-what-is-asked-step-by-step/comments/1532636
  */
-select activity_date as day, count(distinct user_id) as active_users from Activity group by 
-activity_date having activity_date > date_sub('2019-07-27', interval 30 day) and 
-activity_date <= '2019-07-27';
+select activity_date as day, count(distinct user_id) as active_users from Activity group by activity_date 
+having activity_date > date_sub('2019-07-27', interval 30 day) and activity_date <= '2019-07-27';
