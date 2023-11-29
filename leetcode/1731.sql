5c59a4075a2e0ecc055253174bc01bf04faf553f
Rahul Padhy <104157969+imRP26@users.noreply.github.com>
2023-11-29 17:10:30 +0530

    With, JOIN

--- leetcode/1731.sql
+++ leetcode/1731.sql
@@ -11,4 +11,14 @@
 select mgr.employee_id, mgr.name, count(emp.employee_id) as reports_count, round(avg(emp.age)) as 
 average_age from employees emp join employees mgr on emp.reports_to = mgr.employee_id group by 
 employee_id order by employee_id; 
- 
+
+
+
+/*
+ * Simple AF with-clause
+ */
+with t as 
+(select mgr.employee_id, mgr.name, emp.employee_id as emp_id, emp.age from Employees mgr join Employees 
+emp on mgr.employee_id = emp.reports_to)
+select employee_id, name, count(emp_id) as reports_count, round(avg(age)) as average_age from t group by 
+employee_id order by employee_id;
